3922bf7365b89f44d71a664b6713fc28914e587a
julian <jtallar@itba.edu.ar>
2019-11-29 12:50:35 -0300

    Added base function calls

--- app/src/main/java/ar/edu/itba/inge/pab/ui/student/StudentViewModel.java
+++ app/src/main/java/ar/edu/itba/inge/pab/ui/student/StudentViewModel.java
@@ -7,13 +7,22 @@ import java.util.List;
 
 import ar.edu.itba.inge.pab.MyApplication;
 import ar.edu.itba.inge.pab.elements.Project;
+import ar.edu.itba.inge.pab.elements.Student;
 import ar.edu.itba.inge.pab.firebase.ApiRequest;
 import ar.edu.itba.inge.pab.ui.RequestViewModel;
 
-public class StudentViewModel extends RequestViewModel {
+class StudentViewModel extends RequestViewModel {
     LiveData<List<Project>> getFeed() {
         ApiRequest<List<Project>> feedRequest = MyApplication.getInstance().getApiRepository().getFeed();
         requestListeners.add(feedRequest.getListener());
         return Transformations.map(feedRequest.getLiveData(), MyApplication.getTransformFunction());
+    }
+
+    void setStudent(Student student) {
+        MyApplication.getInstance().getApiRepository().setStudent(student);
+    }
+
+    void setProject(Project project) {
+        MyApplication.getInstance().getApiRepository().setProject(project);
     }
 }
