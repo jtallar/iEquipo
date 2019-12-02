429a4f01621aabe6796cbeba1ff6b7a8e71a91d6
mbrula <mbrula@itba.edu.ar>
2019-12-02 17:54:40 -0300

    Notifications duplicated fix

--- app/src/main/java/ar/edu/itba/inge/pab/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/ar/edu/itba/inge/pab/ui/notifications/NotificationsFragment.java
@@ -111,10 +111,8 @@ public class NotificationsFragment extends Fragment {
                 data.clear();
                 for (Notification notification: notifications) {
                     NotificationManager notificationManager = (NotificationManager) MainActivity.getInstance().getSystemService(Context.NOTIFICATION_SERVICE);
-                    notificationManager.cancel(notification.hashCode2());
-                    if (data.contains(notification))
-                        MyApplication.getInstance().getApiRepository().deleteNotification(MainActivity.getLoggedPerson().getId(), notification.getId());
-                    else data.add(0, notification);
+                    notificationManager.cancel(notification.hashCode());
+                    data.add(0, notification);
                 }
                 adapter.notifyDataSetChanged();
             }
